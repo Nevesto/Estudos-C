22187e6217b9403f7c599af01fbd12903192a7ab
nevest <guilhermejcarlucci@hotmail.com>
2024-02-27 21:03:40 -0300

    constants

---
+++ AULA 4 Constants/Constants.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    const int myNum = 15; // A const never changes the value 
+
+    myNum = 10; // gives error
+
+    printf("%d\n", myNum);
+
+    const int minutesPerHour;
+    minutesPerHour = 60; // error 
+}
