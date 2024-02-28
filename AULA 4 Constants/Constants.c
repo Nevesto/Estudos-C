98d82e3a0043d0c274148027a6c10b8a2abb0bd2
nevest <guilhermejcarlucci@hotmail.com>
2024-02-27 21:27:53 -0300

    C Operators

--- AULA 4 Constants/Constants.c
+++ AULA 4 Constants/Constants.c
@@ -3,10 +3,10 @@
 int main() {
     const int myNum = 15; // A const never changes the value 
 
-    myNum = 10; // gives error
+    // myNum = 10; // gives error
 
     printf("%d\n", myNum);
 
     const int minutesPerHour;
-    minutesPerHour = 60; // error 
+    // minutesPerHour = 60; // error 
 }
---
+++ AULA 5 C Operators/operators.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int mySum = 100 + 50; // + (plus) operator
+    printf("%d\n", mySum);
+
+    // you can also use the plus operator 
+
+    int sum1 = 100 + 50;
+    int sum2 = sum1 + 200;
+    int sum3 = sum2 + 10;
+
+    printf("%d\n", sum1);
+    printf("%d\n", sum2);
+    printf("%d\n", sum3);
+
+    int x = 10;
+    x += 5; // The addition assignment operator (+=) adds a value to a variable.
+    printf("%d\n", x);
+}
+
+// Arithmetic Operators
+
+/*
+    + 	Addition 	Adds together two values 	x + y 	
+    - 	Subtraction 	Subtracts one value from another 	x - y 	
+    * 	Multiplication 	Multiplies two values 	x * y 	
+    / 	Division 	Divides one value by another 	x / y 	
+    % 	Modulus 	Returns the division remainder 	x % y 	
+    ++ 	Increment 	Increases the value of a variable by 1 	++x 	
+    -- 	Decrement 	Decreases the value of a variable by 1 	--x
+*/
